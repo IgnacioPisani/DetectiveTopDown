0782e2fdf595e14a0aee40e846a01e847953f5ec
IgnacioPisani <ignaciopisani12@gmail.com>
2026-09-09 16:45:24 -0300

    commit

---
+++ Source/TopDownPrueba/TopDownPrueba.cpp
@@ -0,0 +1,9 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "TopDownPrueba.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, TopDownPrueba, "TopDownPrueba" );
+
+DEFINE_LOG_CATEGORY(LogTopDownPrueba)
+ 
